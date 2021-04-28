ecc7529f6fe78d96897c749e87379bd9d87ba279
totemo <randylozano18@gmail.com>
2021-04-28 16:14:05 +0800

    :sparkles: Implement next-i18next with template

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const { i18n } = require('./next-i18next.config');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer()
+
+module.exports = withBundleAnalyzer();
+module.exports = { 
+  i18n,
+};
---
+++ src/messages/global/index.js
@@ -0,0 +1,10 @@
+export const scope = 'global';
+
+const message = {
+  header: Object.values({
+    key: `${scope}.h1`,
+    default: 'This is header'
+  })
+}
+
+export default message;
