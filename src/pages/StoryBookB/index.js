a39a0685dade9181d19a614dd264e0b15e42a2e8
Francis <macalintalfrancis26@gmail.com>
2021-04-29 16:49:57 +0800

    added webinar components

--- src/pages/StoryBookB/index.js
+++ src/pages/StoryBookB/index.js
@@ -3,6 +3,8 @@ import Layout from '@/components/Elements/Layout'
 import Content from '@/components/Elements/Content'
 import Input from '@/components/Elements/Input'
 import Button from '@/components/Elements/Button'
+import Link from '@/components/Elements/Link'
+import CheckBox from '@/components/Elements/CheckBox'
 
 export default function SampleComponent(){
    
@@ -10,7 +12,7 @@ export default function SampleComponent(){
         <>
                 <Layout>
                     <Content>
-                        <Title >LOGIN</Title>
+                        <Title >StoryBook B</Title>
                         <br>
                         </br>
                         <label>Email</label>
@@ -21,7 +23,14 @@ export default function SampleComponent(){
                         <br></br>
                         <Input type="password" placeholder="Password"></Input>
                         <br/>
+                        <div style={{ margin: '1rem 0', width: '80%', display: 'flex', justifyContent: 'space-between', }}>
+                            <CheckBox>Check this</CheckBox>
+                            <Link href="#" name="Link here"></Link>
+                        </div>
                         <Button>Login</Button>
+                        <div>
+                            Lorem ipsum dolor sit amet? <Link href="#" name="Link somewhere"></Link>
+                        </div>
                     </Content>
                 </Layout>
         </>
