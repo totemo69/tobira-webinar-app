77870e6163db60950bbef6304ad88f682c8a7716
soviet90 <victorinoreymark1990@gmail.com>
2021-05-03 19:00:45 +0800

    fixed translation
    error on change url  http://localhost:3000/en/Signup-example to http://localhost:3000/ja/Signup-example
    
    note :
    
    cannot translate last PR fixed issue

--- src/messages/global/index.js
+++ src/messages/global/index.js
@@ -6,7 +6,55 @@ const message = {
     default: 'This is header'
   }),
 
-  
+  And: Object.values({
+    key: `${scope}.And`,
+    default: 'And'
+}),
+
+AgreeMessage: Object.values({
+    key: `${scope}.AgreeMessage`,
+    default: 'By signing up, you agree to the'
+}),
+ConfirmPassword: Object.values({
+    key: `${scope}.ConfirmPassword`,
+    default: 'Confirm Password'
+}),
+EmailAddress: Object.values({
+    key: `${scope}.EmailAddress`,
+    default: 'Email Address'
+}),
+LoginHere: Object.values({
+    key: `${scope}.LoginHere`,
+    default: 'Log in here',
+}),
+PrivacyPolicy: Object.values({
+    key: `${scope}.PrivacyPolicy`,
+    default: 'Privacy Policy',
+}),
+Password: Object.values({
+    key: `${scope}.Password`,
+    default: 'Password'
+}),
+SignUp: Object.values({
+    key: `${scope}.SignUp`,
+    default: 'Sign Up'
+}),
+TermsOfService: Object.values({
+    key: `${scope}.TermsOfService`,
+    default: 'Terms of Service'
+}),
+Username: Object.values({
+    key: `${scope}.Username`,
+    default: 'Username'
+}),
+
+HaveAccount: Object.values({
+    key: `${scope}.HaveAccount`,
+    default: 'Already have an account?'
+}),
+
+
+
 }
 
 export default message;
