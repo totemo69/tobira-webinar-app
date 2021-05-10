649adbfee347045ea30adee7ab35ce5f04d2396a
soviet90 <victorinoreymark1990@gmail.com>
2021-05-10 19:32:31 +0800

    Update index.js
    fixing conflict to @efelenem PR

--- src/components/Elements/Dropdown/index.js
+++ src/components/Elements/Dropdown/index.js
@@ -24,5 +24,36 @@ const StyledATag = styled.a`
   font-weight: 600;
 `
 
+const menu = (
+  <Menu>
+    <Menu.Item key="0">
+      Profile
+    </Menu.Item>
+    <Menu.Item key="1">
+      Account
+    </Menu.Item>
+    <Menu.Item key="2">
+      Purchase License
+    </Menu.Item>
+    <Menu.Item key="3">
+      Wallet
+    </Menu.Item>
+    <Menu.Divider></Menu.Divider>
+    <Menu.Item key="4">
+      Log out
+    </Menu.Item>
+  </Menu>
+)
+
+
+const Drop = ({ username }) =>{
+  return(
+      <StyledDropdown overlay={menu} placement="bottomRight" trigger={['click']}>
+        <StyledATag>{username} <DownOutlined/></StyledATag>
+      </StyledDropdown>
+  )
+};
+
+Drop.prototype = {}
 
 export default StyledDropdown;
