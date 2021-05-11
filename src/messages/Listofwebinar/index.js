14493aa515691516773fbd0b92974b589df902e3
soviet90 <victorinoreymark1990@gmail.com>
2021-05-11 16:24:51 +0800

    List of webinar translation

---
+++ src/messages/Listofwebinar/index.js
@@ -0,0 +1,39 @@
+export const scope = 'listofwebinar';
+
+const message = {
+  Tittle: Object.values({
+    key: `${scope}.Tittle`,
+    default: "Tittle"
+  }),
+  Listofwebinar: Object.values({
+    key: `${scope}.Listofwebinar`,
+    default: "LIST OF WEBINAR"
+  }),
+  Schedule: Object.values({
+    key: `${scope}.Schedule`,
+    default: "Schedule"
+  }),
+  Attendees: Object.values({
+    key: `${scope}.Attendees`,
+    default: "No. of Attendees"
+  }),
+  Status: Object.values({
+    key: `${scope}.Status`,
+    default: "Status"
+  }),
+  Action: Object.values({
+    key: `${scope}.Action`,
+    default: "Action"
+  }),
+  Search: Object.values({
+    key: `${scope}.Search`,
+    default: "Search"
+  }),
+  SearchPlaceholder: Object.values({
+    key: `${scope}.SearchPlaceholder`,
+    default: "Search webinar title"
+  }),
+  
+}
+
+export default message;
--- src/pages/ListofWebinar/Pages/ListOfWebinar.js
+++ src/pages/ListofWebinar/Pages/ListOfWebinar.js
@@ -1,3 +1,6 @@
+import {useTranslation} from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import message from '@/messages/Listofwebinar'
 import Card from '@/components/Elements/Card';
 import Search from '@/components/Elements/Search';
 import Select from '@/components/Elements/Select';
@@ -5,51 +8,54 @@ import Table from '@/components/Elements/Table'
 import Tittle from '@/components/Elements/Title'
 import { Form } from 'antd';
 
-const column = [
-  {
-    title: 'Tittle',
-    dataIndex: 'tittle',
-    sorter: {
-      multiple: 3,
-    },
-  },
-  {
-    title: 'Schedule',
-    dataIndex: 'Sched',
-    sorter: {
-      multiple: 3,
-    },
-  },
-  {
-    title: 'No. of Attendees',
-    dataIndex: 'Atendies',
-    sorter: {
-      multiple: 3,
-    },
-  },
-  {
-    title: 'Status',
-    dataIndex: 'Status',
-    sorter: {
-      multiple: 3,
-    },
-  },
-  {
-    title: 'Action',
-    dataIndex: 'Action',
-    
-  },
-]
+
 
 export default function ListOfWebinar(){
+  const { t } = useTranslation();
+  const column = [
+    {
+      title: t(message.Tittle),
+      dataIndex: 'tittle',
+      sorter: {
+        multiple: 3,
+      },
+    },
+    {
+      title: t(message.Schedule),
+      dataIndex: 'Sched',
+      sorter: {
+        multiple: 3,
+      },
+    },
+    {
+      title: t(message.Attendees),
+      dataIndex: 'Atendies',
+      sorter: {
+        multiple: 3,
+      },
+    },
+    {
+      title: t(message.Status),
+      dataIndex: 'Status',
+      sorter: {
+        multiple: 3,
+      },
+    },
+    {
+      title: t(message.Action),
+      dataIndex: 'Action',
+      
+    },
+  ]
+  
   return(
     <>
-      <Tittle level={4} style={{color: '#0E71EB'}}>LIST OF WEBINAR</Tittle>
+      <Tittle level={4} style={{color: '#0E71EB'}}>{t(message.Listofwebinar)}</Tittle>
       <Card>
         <Form style={{marginTop: '20px'}}>
-          <Form.Item style={{float: 'right'}} label="Search">
+          <Form.Item style={{float: 'right'}} label={t(message.Search)}>
           <Search
-            placeholder="input search text"
+            placeholder={t(message.SearchPlaceholder)}
             onSearch={value => console.log(value)}
           />
           </Form.Item>
@@ -66,3 +72,9 @@ export default function ListOfWebinar(){
     </>
   )
 }
+
+export const getStaticProps = async ({ locale }) => ({
+  props: {
+    ...await serverSideTranslations(locale, ['translation']),
+  },
+});
