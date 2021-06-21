690e38625d772dff664ea652c765bddae854e3f8
totemo <randylozano18@gmail.com>
2021-06-21 23:53:45 +0800

    :bug: Fix dateTime format for zoom meeting creation

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -2,7 +2,9 @@ import moment from 'moment';
 
 export function CombineDateTime(date, time) {
   if (date && time) {
-    return moment(`${date.format('MM/DD/YYYY')} ${time.format('HH:mm')}`);
+    return moment(
+      `${date.format('MM/DD/YYYY')} ${time.format('HH:mm')}`,
+    ).format('yyyy-MM-DDTHH:mm:ss');
   }
   return null;
 }
