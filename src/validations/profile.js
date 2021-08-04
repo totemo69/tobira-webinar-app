46ca8260765c282364c8f1d7fe11df5d04e6d11f
totemo <randylozano18@gmail.com>
2021-08-04 17:31:39 +0800

    :bug: Fix profile and credentials

--- src/validations/profile.js
+++ src/validations/profile.js
@@ -5,4 +5,17 @@ const validationSchema = Yup.object({
   contact: Yup.string().required('contactNo.requiredField'),
 });
 
+const credentialsValidation = Yup.object({
+  currentPassword: Yup.string().required('password.required'),
+  password: Yup.string()
+    .required('password.required')
+    .min(6, 'passwordMinMax')
+    .max(20, 'passwordMinMax')
+    .matches(/^[a-z0-9]+$/i, 'passwordAlphaNumeric'),
+  confirmPassword: Yup.string()
+    .required('confirmPassword.required')
+    .oneOf([Yup.ref('password'), null], 'confirmPassMatch'),
+});
+
 export default validationSchema;
+export { credentialsValidation };
