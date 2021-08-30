cd6c15f3657820cc1cf381dc05db5433aaecccc0
totemo <randylozano18@gmail.com>
2021-08-30 22:35:31 +0800

    :fire: Add zendesk jp and sentry bugs tracker

--- next.config.js
+++ next.config.js
@@ -5,9 +5,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 const withTM = require('next-transpile-modules')(['react-timezone-select']);
+const { withSentryConfig } = require('@sentry/nextjs');
 
 module.exports = withBundleAnalyzer();
-module.exports = withTM({
+const moduleExports = withTM({
   webpack5: false,
   i18n,
   images: {
@@ -37,3 +38,19 @@ module.exports = withTM({
     return config;
   },
 });
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
