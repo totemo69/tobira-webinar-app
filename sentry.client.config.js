172291808d0048acb9af0745707a6010e326d5ed
totemo <randylozano18@gmail.com>
2021-09-14 02:11:33 +0800

    :fire: Fix wallet and logo

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,9 +7,12 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://602d2689ed8341a984847614fd39ecad@o556902.ingest.sentry.io/5937444',
+  dsn:
+    SENTRY_DSN ||
+    'https://602d2689ed8341a984847614fd39ecad@o556902.ingest.sentry.io/5937444',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+  debug: true,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
